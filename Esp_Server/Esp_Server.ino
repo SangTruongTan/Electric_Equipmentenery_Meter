a4ec1cf9e1ce11caf15ccd17560144369c8abd81
Sang Truong Tan <90814057+SangTruongTan@users.noreply.github.com>
2022-12-16 23:11:03 +0700

    Remove 's' from url to use HTTP instead of HTTPS

--- Esp_Server/Esp_Server.ino
+++ Esp_Server/Esp_Server.ino
@@ -7,7 +7,7 @@ const char* ssid     = "P403_2.4G";
 const char* password = "bontramleba";
 
 // REPLACE with your Domain name and URL path or IP address with path
-String serverName = "https://capnhatdiennuoc.000webhostapp.com/insert2.php";
+String serverName = "http://capnhatdiennuoc.000webhostapp.com/insert2.php";
 
 HTTPClient http;
 WiFiClient wifiClient;
@@ -35,19 +35,21 @@ void setup() {
 void loop() {
   //Check WiFi connection status
   if(WiFi.status()== WL_CONNECTED){
+    // Url
+    String url = serverName + "?Dongdien=" + I + "&Congsuat=" + P + "&Power=" + E + "";
+    Serial.println(url);
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
     // Your Domain name with URL path or IP address with path
-    if(http.begin(wifiClient, serverName)) {
-      // Specify content-type header
-      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      // Url
-      String url = "Dongdien=" + I + "&Congsuat=" + P + "&Power=" + E + "";
-      Serial.println(url);
-      Serial.println("HTTP POST...");
-      int httpCode = http.POST(url);
+    if(http.begin(wifiClient, url)) {
+      Serial.println("HTTP GET...");
+      int httpCode = http.GET();
+      String payload = http.getString(); 
       if(httpCode > 0) {
-        Serial.printf("HTTP code:%d", httpCode);
+        Serial.printf("HTTP code:%d\r\n", httpCode);
+        Serial.println(payload);
       } else {
-        Serial.printf("HTTP GET Failed with code:%d", httpCode);
+        Serial.printf("HTTP GET Failed with code:%d\r\n", httpCode);
       }
       // Free resources
       http.end();
